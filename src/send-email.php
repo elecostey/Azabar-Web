eb4bbb1439b7a1b6bc72c6a645bd35ea34c324b5
Elodie Lecostey <lecostey.e@gmail.com>
2018-02-07 20:48:46 +0100

    implemented recaptcha

--- src/send-email.php
+++ src/send-email.php
@@ -1,6 +1,5 @@
 <?php
 if(isset($_POST['email'])) {
-
     // email addresses where the message will be sent
     $email_info = "lecostey.e@gmail.com";
     $email_sales = "elodie_lecostey@hotmail.com";
@@ -24,7 +23,6 @@ if(isset($_POST['email'])) {
     $message = $_POST['message'];
     $subject = $_POST['subject'];
     $subject_text = $_POST['subject-text'];
-
     $email_subject = $subject_text;
 
     if($subject == 'comment' || $subject == 'information') {
@@ -35,7 +33,6 @@ if(isset($_POST['email'])) {
 
     // Check the user inputs for errors
     $error_message = "";
-
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
     if(!preg_match($email_exp,$email)) {
         $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
@@ -57,36 +54,44 @@ if(isset($_POST['email'])) {
 
     if(strlen($error_message) > 0) {
         died($error_message);
+    } 
+
+     // create email message
+     $email_message = "";
+     function clean_string($string) {
+       $bad = array("content-type","bcc:","to:","cc:","href");
+       return str_replace($bad,"",$string);
+     }
+     $email_message .= "Name: ".clean_string($name)."\n";
+     $email_message .= "Email: ".clean_string($email)."\n";
+     $email_message .= "Phone: ".clean_string($phone)."\n";
+     $email_message .= "Company: ".clean_string($company)."\n";
+     $email_message .= "Subject: ".clean_string($email_subject)."\n";
+     $email_message .= "Message: ".clean_string($message)."\n";
+     
+     // create email headers
+     $headers = 'From: '.$email."\r\n".
+     'Reply-To: '.$email."\r\n" .
+     'X-Mailer: PHP/' . phpversion();
+  
+
+
+    
+    if ( ! empty($_POST)) {
+        $q = http_build_query(array(
+            'secret'    => '6LfBEEUUAAAAAI6NJ6mfvZlRBfPWTJXBnrfrM6iR',
+            'response'  => $_POST['g-recaptcha-response'],
+            'remoteip'  => $_SERVER['REMOTE_ADDR'],
+        ));
+        $result = json_decode(file_get_contents('https://www.google.com/recaptcha/api/siteverify?'.$q));
+        if ($result->success) {
+            @mail($email_to, $email_subject, $email_message, $headers);
+            echo "Thank you for your interest we will get back to you shortly";
+        } else {
+            echo "Please check im not a robot";
+        }
     }
-
-
-    // create email message
-    $email_message = "";
-
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
-
-    $email_message .= "Name: ".clean_string($name)."\n";
-    $email_message .= "Email: ".clean_string($email)."\n";
-    $email_message .= "Phone: ".clean_string($phone)."\n";
-    $email_message .= "Company: ".clean_string($company)."\n";
-    $email_message .= "Subject: ".clean_string($email_subject)."\n";
-    $email_message .= "Message: ".clean_string($message)."\n";
-
-
-    // create email headers
-    $headers = 'From: '.$email."\r\n".
-    'Reply-To: '.$email."\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-    // Send email
-    @mail($email_to, $email_subject, $email_message, $headers)
-
 ?>
-
-Thank you for your interest we will get back to you shortly
 
 <?php
 }
