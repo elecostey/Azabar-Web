650131e42f46073e758028ae46bde09c883bcfeb
Elodie Lecostey <lecostey.e@gmail.com>
2018-02-15 00:36:17 +0100

    finished first section of business solutions landing page

--- src/javascripts/functions/swiper.js
+++ src/javascripts/functions/swiper.js
@@ -22,6 +22,16 @@ const swiper = function() {
             prevEl: '.swiper-button-prev',
         }
     });
+
+    new Swiper('.swiper-container--text', {
+        direction: 'vertical',
+        slidesPerView: 'auto',
+        freeMode: true,
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+        mousewheel: true,
+      });
 };
 
 export default swiper;
